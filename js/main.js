c671cae4f6557ae822c746277a666c70f41fec4d
David Martinez <d.martinez@innovation.es>
2019-03-27 13:46:47 +0100

    calendar changed to jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,74 @@
 $( document ).ready(function() {
-    // moment.locale('es')
-    var now = moment();
-    console.log(now.format('MMMM Do YYYY, h:mm:ss a'));
-  });
+    // Uncomment this line to change the language to english
+    // moment.locale('en')
+    let now = moment().startOf('month')
+    console.log(now.format('MMMM Do YYYY, h:mm:ss a d'))
+
+    let WeekOrder = moment(now)
+    let dayDiff = WeekOrder.startOf('week')
+    WeekOrder.subtract(dayDiff,'days')
+    
+    // Create the table headers
+    for (let i = 0; i < 7; i++) {
+
+        let dayText = WeekOrder.format('dd') 
+        let th = document.createElement("th")
+        th.innerHTML= dayText.charAt(0)
+
+        $('#calendar-head').append(th)
+
+        WeekOrder.add(1,'day')
+    }
+
+    updateCalendar(now)
+
+    $( "#calendar-header .fa-angle-left" ).click(function() {
+        updateCalendar(now.subtract(2, 'months'))    
+    })
+    $( "#calendar-header .fa-angle-right" ).click(function() {
+        updateCalendar(now.add(0, 'months'))
+    })
+    
+})
+
+function updateCalendar(now) {
+
+    // Set the month selector text and empty the table
+    $('#calendar-header h3').html(now.format('MMMM YYYY'))
+    $('#calendar-body').empty()
+
+    let dayOfMonth = now
+    let lastDay = parseInt(now.endOf('month').format('D'))
+
+    now.startOf('month')
+
+    let tr = document.createElement("tr")
+    let lang = moment.locale()
+    
+    //in relation to the language, we change the day week starts(monday/sunday)
+    let weekStartDay = 0
+    if (moment.locale() == "en") {weekStartDay = 6}
+
+    let dayNumber = 1
+    for (let i = 0; i < lastDay; i++) {
+        
+        let td = document.createElement("td")
+
+        td.innerHTML = dayNumber
+        dayNumber++
+        tr.append(td)
+
+        if ( dayOfMonth.format('d') == weekStartDay ) {
+            // Insert empty td to make the calendar start where it should
+            while (tr.childElementCount < 7) {
+                tr.insertBefore(document.createElement("td"), tr.firstChild)
+            }
+            $('#calendar-body').append(tr)
+            tr = document.createElement("tr")
+        }
+
+        dayOfMonth.add('1','day')
+    }
+    $('#calendar-body').append(tr)
+    
+}
